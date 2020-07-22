d94785b1e23a03e1c2c3347a92393f395368d16f
lee-shun-webull <2015097272@qq.com>
2020-07-22 15:46:15 +0800

    first commit

---
+++ code/PID_controller/Robot.py
@@ -0,0 +1,100 @@
+# -----------
+# User Instructions
+#
+# Implement a P controller by running 100 iterations
+# of robot motion. The desired trajectory for the 
+# robot is the x-axis. The steering angle should be set
+# by the parameter tau so that:
+#
+# steering = -tau * crosstrack_error
+#
+# You'll only need to modify the `run` function at the bottom.
+# ------------
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ------------------------------------------------
+# 
+# this is the Robot class
+#
+
+class Robot(object):
+    def __init__(self,length=20.0):
+        """
+        creat robot and initializes location/orientation to 0,0,0
+        """
+        self.x = 0.0
+        self.y = 0.0
+        self.length = length
+        self.orientation = 0.0
+        self.steering_noise = 0.0
+        self.distance_noise = 0.0
+        self.steering_dirft = 0.0 #漂移
+
+    def set(self,x,y,orientation):
+        """
+        Sets a robot coordinate
+        unit:rad
+        """
+        self.x = x
+        self.y = y
+        self.orientation = orientation % (2.0 * np.pi)
+
+    def set_noise(self,steering_noise,distance_noise):
+        """
+        Sets the noise parameters.
+        """
+        # makes it possible to change the noise parameters
+        # this is often useful in particle filters
+        self.steering_noise = steering_noise
+        self.distance_noise = distance_noise
+
+    def set_steering_drift(self,drift):
+        """
+        Sets the systematical steering drift parameter
+        """
+        self.steering_drift = drift
+
+    def move(self,steering,distance,tolerance = 0.001,
+             max_steering_angel=np.pi/4.0):
+        """
+        Steering = front wheel steering angle, limited by the max_
+        distance = total_distance driven, must be non-negative
+        """
+        if steering > max_steering_angle:
+            steering = max_steering_angle
+        if steering < -max_steering_angle:
+            steering = -max_steering_angle
+        if distance < 0.0:
+            distance = 0.0
+
+        #apply the noise
+        steering2 = random.gauss(steering,self.steering_noise)
+        distance2 = random.gauss(distance,self.distance_noise)
+
+        #apply steering drift
+        steering2 += self.steering_drift
+
+        #execute motion
+        turn = np.tan(steering2) * distance2 / self.length
+
+        if abs(turn) < tolerance:
+            #approximate by the straight line motion
+            self.x += distance2 * np.cos(self.orientation)
+            self.y += distance2 * np.sin(self.orientation)
+            self.orientation = (self.orientation + trun) % (2.0 * np.pi)
+        else:
+            #approximate bicycle model for motion
+            radius = distance2 / turn
+            cx = self.x - (np.sin(self.orientation) * radius)
+            cy = self.y + (np.cos(self.orientation) * radius)
+            self.orientation = (self.orientation + turn) % (2.0 * np.pi)
+            self.x = cx + (np.sin(self.orientation) * radius)
+            self.y = cy - (np.cos(self.orientation) * radius)
+
+    def __repr__(self):
+        return '[x=%.5f y=%.5f orient=%.5f]' % \
+                (self.x, self.y, self.orientation)
+
